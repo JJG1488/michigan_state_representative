b2257b5dfa21e4feddf9c5a5bdbd471d1f6f1154
James Gault <74686199+JJG1488@users.noreply.github.com>
2026-03-02 14:38:11 -0500

    updated photo on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -30,7 +30,7 @@ export default function AboutPage() {
             </div>
             <div className="relative aspect-[3/4] rounded-2xl overflow-hidden">
               <Image
-                src="/candace-calloway-headshot.png"
+                src="/cc.JPG"
                 alt={`${theme.candidate.name} headshot`}
                 fill
                 sizes="(max-width: 768px) 100vw, 50vw"
