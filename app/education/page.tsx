761d17f921847d37bb91a34fe69511e641c098cc
James Gault <74686199+JJG1488@users.noreply.github.com>
2026-03-02 15:31:32 -0500

    updated education photo

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -9,5 +9,5 @@ export const metadata: Metadata = {
 
 export default function EducationPage() {
   const pillar = theme.issues.pillars[1];
-  return <PillarPage pillar={pillar} heroImage="/education.png" />;
+  return <PillarPage pillar={pillar} heroImage="/education2.JPEG" />;
 }
